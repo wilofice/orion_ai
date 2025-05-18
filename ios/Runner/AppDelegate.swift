1b7753c8001baba82d02701cf6e718a17a9717d6
ALAHASSA Généreux <wilofice@gmail.com>
2025-05-18 18:42:56 +0200

    Update iOS platform versions, Firebase dependencies, AppDelegate.swift, and Info.plist configurations

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,23 @@
 import Flutter
 import UIKit
-
+import GoogleSignIn
+import Firebase
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure() // Initialize Firebase
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func application(
+      _ app: UIApplication,
+      open url: URL,
+      options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
 }
